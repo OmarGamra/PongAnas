3fc086291878ec4de19721d2aebf1b8bf0499e96
OmarGamra <gamraomar03@gmail.com>
2020-06-16 12:46:51 +0100

    pong

---
+++ Pong/Assets/Ball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed,0);
+    }
+
+}
+
+    
+    
---
+++ Pong/Assets/Checkpoint2.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+        void Start()
+    {
+        Debug.Log(Sum(1,2));
+        Debug.Log(Prod(6,3));
+        Debug.Log(Div(5,7));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    float Sum(int a, int b)
+    {
+        return a + b;
+    }
+    float Prod(int a, int b)
+    {
+        return a * b;
+    }
+    float Div(int a, int b)
+    { 
+        if(b == 0)
+        { Debug.Log("ERROR");
+            return 0;
+        }
+        else return a / b;
+        }
+}
