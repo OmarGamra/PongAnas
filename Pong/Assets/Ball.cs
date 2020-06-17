eb11177e650fd5ca092fd563bc4c921dcfadc6ee
OmarGamra <gamraomar03@gmail.com>
2020-06-17 11:19:33 +0100

    finish pong

--- Pong/Assets/Ball.cs
+++ Pong/Assets/Ball.cs
@@ -2,18 +2,73 @@
 using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 
 public class Ball : MonoBehaviour
 {
+    public Text scorerightText;
+    public Text scoreleftText;
+    int scoreRight;
+    int scoreLeft;
     public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(speed,0);
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0);
     }
+    float hitFactor(Vector2 ballPos, Vector2 racketPos,
+                float racketHeight)
+    {
+        // ascii art:
+        // ||  1 <- at the top of the racket
+        // ||
+        // ||  0 <- at the middle of the racket
+        // ||
+        // || -1 <- at the bottom of the racket
+        return (ballPos.y - racketPos.y) / racketHeight;
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {  // Hit the left Racket?
+        if (col.gameObject.name == "RacketLeft")
+        {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
 
-}
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(1, y).normalized;
 
-    
-    
+            // Set Velocity with dir * speed
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+
+        // Hit the right Racket?
+        if (col.gameObject.name == "RacketRight")
+        {
+            // Calculate hit Factor
+            float y = hitFactor(transform.position,
+                                col.transform.position,
+                                col.collider.bounds.size.y);
+
+            // Calculate direction, make length=1 via .normalized
+            Vector2 dir = new Vector2(-1, y).normalized;
+
+            // Set Velocity with dir * speed
+
+            GetComponent<Rigidbody2D>().velocity = dir * speed;
+        }
+        if (col.gameObject.name == "Wallright")
+        {
+            scoreLeft++;
+            scoreleftText.text = scoreLeft.ToString();
+        }
+        if (col.gameObject.name == "Wallleft")
+        {
+            scoreRight++;
+            scorerightText.text = scoreRight.ToString();
+        }
+    }
+}//end
